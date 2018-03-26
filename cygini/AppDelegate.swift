dd55e43c13376d7b72eaa5062b79a245cc053eb7
Rahul Ranjan <rahul.rrixe@gmail.com>
2018-03-26 22:13:54 +0800

    create the launch screen

--- cygini/AppDelegate.swift
+++ cygini/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+
+    let homeVC = HomeViewController(collectionViewLayout: UICollectionViewFlowLayout())
+    window?.rootViewController = UINavigationController(rootViewController: homeVC)
+
     return true
   }
 
---
+++ cygini/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  cygini
+//
+//  Created by Rahul Ranjan on 3/26/18.
+//  Copyright © 2018 Rahul Ranjan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = UIColor.red
+  }
+}
+
